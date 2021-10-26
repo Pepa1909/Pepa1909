cd06753d7f941c77833ea0847805dee6a353917b
Pepa1909 <93058958+Pepa1909@users.noreply.github.com>
2021-10-26 15:21:18 +0200

    Delete 2. hodina_3.py

--- 2. hodina_3.py
+++
@@ -1,6 +0,0 @@
-#funkce(argument)
-#print(int("0xFF", base=16)) 
-    #převede číslo z různé soustavy do desítkové, druhý argument označuje sestavu, ve které je číslo
-    #base=16 je pojmenovaný argument
-#print(1,2,3,4,5, sep="# #", end="#\n\n")
-#želví grafika nyní
