03b5ba8727925b7db6ada996d518f4a08b60f46f
Josef Vojkovský <pepavoj@email.cz>
2021-12-08 13:51:43 +0100

    -

--- du2/du2_josef_vojkovsky.py
+++ du2/du2_josef_vojkovsky.py
@@ -1,7 +1,99 @@
+# naimportování potřebných modulů
 import csv
-from os import write
-with open("hm.csv", encoding="UTF-8") as csvfile, open("hm_out.csv", "w", encoding = "UTF-8") as csvoutfile:
+from os import stat
+import statistics
+import math
+
+# definování několika funkcí, které se opakují pro oba výstupy
+
+# funkce na započítání a správné zapsání posledního řádku
+def zbytek(hodnota, prumer, radek_list, writer):
+    if len(hodnota) > 0:
+        prumer = statistics.mean(hodnota)
+        writer.writerow(radek_list[0] + ["   "+ format(prumer, ".4f")])
+
+# funkce, která ošetřuje chybějící hodnotu průtoku (= poslední sloupec)
+def try_block(hodnota, row):
+    try:
+        hodnota.append(float(row[-1]))
+    except ValueError:
+        print(f"chybí data za den {row[2:-1]}. Prosím doplňte.")
+        quit()
+
+#funkce, která ošetřuje duplicitní dny ve vstupu
+def duplikat(row):
+    if len(radek_tyden_list) > 1 and radek_tyden_list[-2] == radek_tyden_list[-1]:
+        print(f"datový soubor obsahuje duplikát, den {row[2:-1]} je zapsaný dvakrát. Program se ukončí")
+        quit()
+
+# funkce na extrémy
+def extremy(max_prutok_list, min_prutok_list):
+    max_prutok = max_prutok_list[0]
+    for hodnoty_max_prutok in max_prutok_list:
+        if hodnoty_max_prutok > max_prutok:
+            max_prutok = hodnoty_max_prutok
+    min_prutok = min_prutok_list[0]
+    for hodnoty_min_prutok in min_prutok_list:
+        if hodnoty_min_prutok < min_prutok:
+            min_prutok = hodnoty_min_prutok
+    max_prutok_list =[max_prutok]
+    min_prutok_list = [min_prutok]
+    return max_prutok,min_prutok
+
+
+# otevření souborů na sedmidenní průměr
+with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_7dni.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
     reader = csv.reader(csvfile, delimiter = ",")
     writer = csv.writer(csvoutfile)
-    for row in reader:
-        writer.writerow([row[-1]])
+    # vytvoření listů na ukládání klíčových hodnot
+    hodnota_prutoku_tyden = []
+    radek_tyden_list = []
+    max_prutok_list = []
+    min_prutok_list = []
+    # for cyklus, který projede celý .csv soubor
+    for row_tyden in reader:
+        max_prutok_list.append(float(row_tyden[-1]))
+        min_prutok_list.append(float(row_tyden[-1]))
+        radek_tyden_list.append(row_tyden[0:-1])
+        try_block(hodnota_prutoku_tyden,row_tyden)
+        # vložený for cyklus, který se stane (počet řádků ve vstupním souboru)/7-krát, ale dělení je celočíselné
+        for _ in range(len(hodnota_prutoku_tyden)//7):
+            duplikat(row_tyden)
+            # pokud je délka seznamu hodnot 7, udělá se z nich průměr, řádek se vytiskne do výstupu, seznamy se vyčistí a jede se znovu
+            if len(hodnota_prutoku_tyden) == 7:
+                prumer_tyden = statistics.mean(hodnota_prutoku_tyden)
+                writer.writerow(radek_tyden_list[0] + ["   "+ format(prumer_tyden, ".4f")])
+                hodnota_prutoku_tyden=[]
+                radek_tyden_list = []
+                extremy(max_prutok_list,min_prutok_list)
+    # toto se stane, když v souboru nezbývá 7 dnů
+    zbytek(hodnota_prutoku_tyden, prumer_tyden, radek_tyden_list, writer)
+
+# otevření souborů na roční průměr
+with open("vstup.csv", encoding="UTF-8") as csvfile, open("vystup_rok.csv", "w", encoding = "UTF-8", newline="") as csvoutfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    writer = csv.writer(csvoutfile)
+    # vytvoření seznamů, oproti týdnu je tu i seznam, který uchovává o jaký rok se právě jedná
+    hodnota_prutoku_rok = []
+    radek_rok_list = []
+    rok = []
+    # for cyklus, který projede celý soubor
+    for row_rok in reader:
+        radek_rok_list.append(row_rok[0:-1])
+        try_block(hodnota_prutoku_rok,row_rok)
+        rok.append(int(row_rok[2]))
+        # podobně jako u týdenního průměru, toto se stane (počet řádků ve vstupním souboru)/365-krát, zaokrouhleno nahoru
+        for _ in  range(math.ceil(len(hodnota_prutoku_rok)/365)):
+            duplikat(row_tyden)
+            # když jsou v seznamu roků alespoň 2 hodnoty a poslední se nerovná předposlední, nastane odebrání posledního řádku (už jiný rok) a vypočítá se
+            # průměr z předešlých hodnot; pak se seznamy vyčistí, ale ponechá se v nich odebraný řádek z předešlého roku (jedná se totiž o 1. leden nového roku)
+            if len(rok) > 1 and rok[-2] != rok[-1]:
+                prvni_hodnota=hodnota_prutoku_rok.pop()
+                prvni_radek=radek_rok_list.pop()
+                prumer_rok = statistics.mean(hodnota_prutoku_rok)
+                writer.writerow(radek_rok_list[0]+["   "+ format(prumer_rok, ".4f")])
+                hodnota_prutoku_rok = [prvni_hodnota]
+                rok = []
+                radek_rok_list = [prvni_radek]
+    # provede se pro poslední rok v záznamu, jinak by chyběl (druhá část podmínky z řádku 69 už nemůže nastat)
+    zbytek(hodnota_prutoku_rok, prumer_rok, radek_rok_list, writer)
---
+++ du2/readme.md
@@ -0,0 +1,14 @@
+# Manuál pro program
+
+Tento program umí načíst soubor s příponou .csv nacházející se ve stejné složce. Soubor se musí jmenovat vstup.csv, jeho řádky musí být v následujícm formátu, hodnoty rozděleny čárkou:
+
+databázové číslo,označení typu dat,rok,měsíc,den,průměrný denní průtok (m3/s)
+
+příklad:
+238000,QD,2016,02,13,   8.2400
+
+Program následně soubor přečte, a vytvoří 2 nové soubory: vystup_7dni.csv a vystup_rok.csv. Pokud nalezne chybějící hodnoty či zduplikované řádky, napíše, kde se daná chyba nachází, a ukončí se.
+
+V souboru vystup_7dni.csv se budou vyskytovat průměrné hodnoty za každých 7 dní od začátku měření s tím, že v souboru bude datum prvního dne daného týdnu; když by na poslední týden nevyzbylo 7 dní, vezme všechny zbylé dny.
+
+Soubor vystup_rok.csv bude obsahovat průměrné hodnoty za každý rok, datum roku bude datum 1. naměřené hodnoty v ten rok (pravděpodobně 1. leden). Program začíná počítat nový rok vždycky se změnou třetího sloupce (rok), ale nepozná, jestli nějaký den v roce chybí. 
